dab2892857454534772fc50c0f2db9b914f73bb2
vishal veerareddy <vishalveera.reddy@servicenow.com>
2026-01-17 23:24:51 -0800

    added homebrew support

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 
+if (process.argv.includes('--version') || process.argv.includes('-v')) {
+  const pkg = require('../package.json');
+  console.log(pkg.version);
+  process.exit(0);
+}
+
 require("../index.js");
